d16570318be3aa6bdb9274ba92b258309b42677f
roshan <roshanvj29072002@gmail.com>
2024-11-04 13:06:53 +0530

    imported icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './app.scss';
-import About from './Components/about/About';
-import Hero from './Components/Hero/Hero';
-import Navbar from './Components/Navbar/Navbar';
-import Parallax from './Components/Parallax/Parallax';
+import About from './components/about/About';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Parallax from './components/Parallax/Parallax';
 import './index.css'
 import Projects from './components/Projects/Projects';
 import { TimelineDemo } from './TimelinePage';
@@ -23,7 +23,7 @@ function App() {
         <Parallax />
       </section>
       <section id="SKILLS">
-        <About />
+       <About />
       </section>
       <section className="section1" >
         <Projects />
