d7d5ab894f174fbc40d11f01fcc7e0ee639f900f
roshan <roshanvj29072002@gmail.com>
2024-11-20 16:34:14 +0530

    hero page responsiveness added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Expose server to the network
+    port: 5173,      // Ensure this matches your desired port
+  },
 })
+
+
